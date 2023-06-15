c425b989d00ad9b610df4ec82b854a9f3b3cb7c2
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-06-16 01:10:28 +0200

    made the plugin disable entirely /server and allow all commands to console.

--- src/main/java/fr/eirb/caslogin/CasLogin.java
+++ src/main/java/fr/eirb/caslogin/CasLogin.java
@@ -1,11 +1,16 @@
 package fr.eirb.caslogin;
 
 import com.google.inject.Inject;
+import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.permission.PermissionsSetupEvent;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.permission.Tristate;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.ConsoleCommandSource;
 import com.velocitypowered.api.proxy.ProxyServer;
+import fr.eirb.caslogin.commands.CasCommand;
 import fr.eirb.caslogin.manager.ConfigurationManager;
 
 import java.nio.file.Path;
@@ -30,6 +35,25 @@ public class CasLogin {
 	}
 	@Subscribe
 	public void onProxyInit(ProxyInitializeEvent ev){
+		logger.info("Loading plugin...");
+		registerCommands();
+		logger.info("Plugin successfully loaded!");
+	}
 
+	@Subscribe(order = PostOrder.FIRST)
+	public void disableServerCommand(PermissionsSetupEvent ev){
+		ev.setProvider(subject -> permission -> {
+			if(subject instanceof ConsoleCommandSource)
+				return Tristate.TRUE;
+			if(permission.equals("velocity.command.server"))
+				return Tristate.FALSE;
+			return Tristate.UNDEFINED;
+		});
+	}
+
+	private void registerCommands() {
+		logger.info("Loading commands...");
+		proxy.getCommandManager().register(CasCommand.createCasCommand(proxy));
+		logger.info("Finished loading commands.");
 	}
 }
