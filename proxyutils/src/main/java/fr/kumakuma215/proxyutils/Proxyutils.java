8de4cb6c7dd32b57c42f04cdc68db0a4aedf41bc
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-09-04 15:06:26 +0200

    added dependency to caslogin plugin.

--- proxyutils/src/main/java/fr/kumakuma215/proxyutils/Proxyutils.java
+++ proxyutils/src/main/java/fr/kumakuma215/proxyutils/Proxyutils.java
@@ -3,15 +3,21 @@ package fr.kumakuma215.proxyutils;
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Dependency;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.ProxyServer;
+import fr.eirb.caslogin.CasLogin;
+import fr.eirb.caslogin.Constants;
 import fr.kumakuma215.proxyutils.commands.AlertCommand;
 import org.slf4j.Logger;
 
 @Plugin(
 		id = "proxyutils",
 		name = "proxyutils",
-		version = BuildConstants.VERSION
+		version = BuildConstants.VERSION,
+		dependencies = {
+				@Dependency(id = "caslogin")
+		}
 )
 public class Proxyutils {
 
