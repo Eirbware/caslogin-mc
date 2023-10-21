7c01f2cb3b892304a9a5544560079568b4286e41
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2023-10-21 13:23:57 +0200

    some refactoring

--- velocity/src/main/java/fr/eirb/caslogin/utils/ProxyUtils.java
+++ velocity/src/main/java/fr/eirb/caslogin/utils/ProxyUtils.java
@@ -3,28 +3,32 @@ package fr.eirb.caslogin.utils;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Collection;
 
 public final class ProxyUtils {
 	private static Class<?> velocityClass = null;
 	private static Class<?> connectedPlayerClass = null;
 	private static Method unregisterConnectionMethod = null;
 
-	public static void unregisterConnection(ProxyServer server, Player player) {
+	private static void initVelocityClass(ProxyServer server){
 		if (velocityClass == null)
 			velocityClass = server.getClass();
+	}
 
+	private static void initConnectedPlayerClass(Player player){
 		if (connectedPlayerClass == null)
 			connectedPlayerClass = player.getClass();
+	}
 
-		if (unregisterConnectionMethod == null) {
-			try {
-				unregisterConnectionMethod = velocityClass.getDeclaredMethod("unregisterConnection", connectedPlayerClass);
-			} catch (NoSuchMethodException e) {
-				throw new RuntimeException(e);
-			}
-		}
+	public static void unregisterConnection(ProxyServer server, Player player) {
+		initVelocityClass(server);
+		initConnectedPlayerClass(player);
+		initUnregisterConnectionMethod();
 
 		unregisterConnectionMethod.setAccessible(true);
 		try {
@@ -33,6 +37,16 @@ public final class ProxyUtils {
 			throw new RuntimeException(e);
 		} finally {
 			unregisterConnectionMethod.setAccessible(false);
+		}
+	}
+
+	private static void initUnregisterConnectionMethod() {
+		if (unregisterConnectionMethod == null && velocityClass != null && connectedPlayerClass != null) {
+			try {
+				unregisterConnectionMethod = velocityClass.getDeclaredMethod("unregisterConnection", connectedPlayerClass);
+			} catch (NoSuchMethodException e) {
+				throw new RuntimeException(e);
+			}
 		}
 	}
 }
