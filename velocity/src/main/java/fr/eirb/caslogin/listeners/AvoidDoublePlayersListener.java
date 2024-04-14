b7205fad531a1d7b69ac3e335cc964655f84ce87
Khalifa Samuel <samuel.khalifa@bordeaux-inp.fr>
2024-04-14 18:58:54 +0200

    chore: change message

--- velocity/src/main/java/fr/eirb/caslogin/listeners/AvoidDoublePlayersListener.java
+++ velocity/src/main/java/fr/eirb/caslogin/listeners/AvoidDoublePlayersListener.java
@@ -2,6 +2,7 @@ package fr.eirb.caslogin.listeners;
 
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.PreLoginEvent;
+import fr.eirb.caslogin.CasLogin;
 import fr.eirb.caslogin.utils.ProxyUtils;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 
@@ -11,7 +12,7 @@ public class AvoidDoublePlayersListener {
 		if (ProxyUtils.usernameExists(ev.getUsername()))
 			ev.setResult(PreLoginEvent.PreLoginComponentResult.denied(MiniMessage
 					.miniMessage()
-					.deserialize("<red>Ce joueur est déjà connecté, veuillez changer de compte Minecraft!"))
+					.deserialize("<red>Ce joueur est déjà connecté, veuillez changer de compte Minecraft !"))
 			);
 	}
 }
